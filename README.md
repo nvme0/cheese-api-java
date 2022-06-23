38f60e7c52ff4062003f1c0461d8608197b42f23
jeffsummers <jeff.summers@patientzero.com.au>
2022-06-23 11:24:40 +1000

    Add name for Swagger API Docs and update README.md

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 1. Run `docker-compose up` to start the postgreSQL server
 
 2. Depending on your IDE, start the Cheese API server in `/api`
+
+## Swagger UI
+
+To view Swagger UI, start the API server and go to `http://localhost:8080/swagger-ui/index.html`.
+
+To view the raw openapi JSON, go to `http://localhost:8080/v3/api-docs`
--- api/src/main/java/com/cheese/api/ApiApplication.java
+++ api/src/main/java/com/cheese/api/ApiApplication.java
@@ -1,9 +1,13 @@
 package com.cheese.api;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @OpenAPIDefinition
@@ -13,4 +17,12 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		OpenAPI openapi = new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Cheese API"));
+
+		return openapi;
+	}
 }
