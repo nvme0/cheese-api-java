a528764255f387faed90eddd06928c010c95b345
jeffsummers <jeff.summers@patientzero.com.au>
2022-06-23 17:55:17 +1000

    Set allowed CORS methods to all

--- api/src/main/java/com/cheese/api/ApiApplication.java
+++ api/src/main/java/com/cheese/api/ApiApplication.java
@@ -35,7 +35,10 @@ public class ApiApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry
+						.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("*");
 			}
 		};
 	}
